69ecedca73535cd5dada66c6e151befaf4b0428a
yanto28jelero <yanto28jelero@gmail.com>
2018-12-05 16:24:13 -0800

    tentang perancang

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -290,8 +290,8 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h2 class="section-heading text-uppercase">Tim Kami</h2>
+            <h3 class="section-subheading text-muted">Kami terdiri dari beberapa orang.</h3>
           </div>
         </div>
         <div class="row">
@@ -299,7 +299,7 @@
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="/ujiankanlah/hotel/img/team/1.jpg" alt="">
               <h4>Kay Garland</h4>
-              <p class="text-muted">Lead Designer</p>
+              <p class="text-muted">Pendesain Utama</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -323,7 +323,7 @@
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="/ujiankanlah/hotel/img/team/2.jpg" alt="">
               <h4>Larry Parker</h4>
-              <p class="text-muted">Lead Marketer</p>
+              <p class="text-muted">Kepala Bagian Marketing</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -347,7 +347,7 @@
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="/ujiankanlah/hotel/img/team/3.jpg" alt="">
               <h4>Diana Pertersen</h4>
-              <p class="text-muted">Lead Developer</p>
+              <p class="text-muted">Pengembang Utama</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -370,7 +370,7 @@
         </div>
         <div class="row">
           <div class="col-lg-8 mx-auto text-center">
-            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+            <p class="large text-muted">Setiap orang mempunyai tugasnya masing masing dan tentunya juga kita tidak bekerja sewndiri-sendiri. Ada berbagai masukan dan bantuan yang datang.</p>
           </div>
         </div>
       </div>
